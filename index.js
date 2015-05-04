5008c13b65a59e78d8736b374d570768a41af40b
y-kurami <yosuke.kurami@gmail.com>
2015-05-05 01:22:03 +0900

    Add fileUtil

---
+++ index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var remote = require('remote');
+var fileUtil = remote.require('./lib/fileUtil');
+
+fileUtil.fetchReadmeList(function(err, matches) {
+  if(!err) document.write(matches.join());
+});
+
